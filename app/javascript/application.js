47a290b1d3297cac83ee369f6b277aeb8766341d
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-03-16 23:41:12 +0500

    Add mobile responsiveness to the home page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,19 @@ import "controllers";
 //= require jquery3
 //= require popper
 //= require bootstrap
+
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const closeBtn = document.querySelector(".close-btn");
+const navList = document.querySelector(".nav_items");
+
+hamburgerBtn.addEventListener("click", function () {
+  navList.style.display = "block";
+  hamburgerBtn.style.display = "none";
+  closeBtn.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function () {
+  navList.style.display = "none";
+  hamburgerBtn.style.display = "block";
+  closeBtn.style.display = "none";
+});
