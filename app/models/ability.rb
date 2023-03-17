82b75bb468e28675e2d7f921424f807cb454a4a9
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-03-17 11:06:41 +0100

    Add file for cancan

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    can :read, RecipeItem
+
+    return unless user.present?
+
+    can :manage, Food, user_id: user.id
+    can :manage, RecipeItem, user_id: user.id
+    can :manage, RecipeFood
+
+    return unless user.admin?
+    can :manage, :all
+
+  end
+end
